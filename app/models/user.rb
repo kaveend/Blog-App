1d1298ac702b38c59a3ea3997dba15402d80213b
Kaveend <jayjayasinghe9@gmail.com>
2018-12-05 20:35:10 +0000

    Add User Data to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+    has_many :articles
+    before_save { self.email = email.downcase }
     validates :username, presence: true,
                         uniqueness: {case_sensitive: false },
                         length: { minimum: 8, maximum: 30}
     
-    VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105}, 
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
