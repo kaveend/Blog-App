fdb9e02609dd6d460fae68c84df8500600c9464e
Kaveend <jayjayasinghe9@gmail.com>
2019-01-04 21:58:21 +0000

    User Restriction 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -58,4 +59,10 @@ class UsersController < ApplicationController
     
     def set_user
        @user = User.find(params[:id]) 
+    end
+    
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "What are You Doing? You Can Only Edit Your OWN account!"
+            redirect_to root_path
     end
